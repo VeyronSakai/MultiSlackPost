3ba75011136ecd24ecbe868cb202b8505906a287
VeyronSakai <yuukisakai123@gmail.com>
2023-06-24 16:18:28 +0900

    add PostUseCase

--- MultiSlackPost/Command/PostCommand.cs
+++ MultiSlackPost/Command/PostCommand.cs
@@ -2,8 +2,7 @@ using System.Diagnostics.CodeAnalysis;
 using Cocona;
 using Microsoft.Extensions.Logging;
 using MultiSlackPost.Domain;
-using SlackAPI;
-using ZLogger;
+using MultiSlackPost.UseCase;
 
 namespace MultiSlackPost.Command;
 
@@ -16,44 +15,7 @@ public class PostCommand
         [FromService] IConfigRepository configRepository,
         [FromService] ILogger<ChannelCommand> logger)
     {
-        if (!configRepository.Exists())
-        {
-            throw new CommandExitedException("configuration does not exist", 1);
-        }
-
-        var config = await configRepository.GetAsync();
-
-        var tasks = new List<Task>();
-
-        foreach (var (workspace, channels) in config.Channels)
-        {
-            config.Tokens.TryGetValue(workspace, out var token);
-            if (token == null)
-            {
-                throw new CommandExitedException($"token is not registered. workspace = {workspace}", 1);
-            }
-
-            foreach (var channel in channels)
-            {
-                tasks.Add(SendAsync(channel, workspace, body, token, logger));
-            }
-        }
-
-        await Task.WhenAll(tasks);
-    }
-
-    private static async Task SendAsync(string channel, string workspace, string body, string token, ILogger logger)
-    {
-        var slackClient = new SlackTaskClient(token);
-        var response = await slackClient.PostMessageAsync(channel, body);
-        if (response.ok)
-        {
-            logger.ZLogInformation($"Successfully posted message to #{channel} in workspace {workspace}");
-        }
-        else
-        {
-            throw new CommandExitedException(
-                $"An error occurred when posting to #{channel} in workspace {workspace}. Error: {response.error}", 1);
-        }
+        var useCase = new PostUseCase(configRepository, logger);
+        await useCase.PostToAllChannelsAsync(body);
     }
 }
